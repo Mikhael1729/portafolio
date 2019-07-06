47178f4cf564e4f60357afe7f104ca908cc955a0
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-07-05 19:08:11 -0700

    Using GridView.builder to do two columns list.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,49 +13,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    final List<Point> row1 = [];
-    final List<Point> row2 = [];
-
-    for (var i = 0; i < points.length; i++) {
-      final point = points[i];
-      if (i % 2 == 0)
-        row1.add(point);
-      else
-        row2.add(point);
-    }
-
-    return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: SingleChildScrollView(
-          child: Row(children: [
-            Expanded(
-                flex: 1,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:
-                        row1.map((point) => _buildPointCard(point)).toList())),
-            Expanded(
-              flex: 1,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:
-                      row2.map((point) => _buildPointCard(point)).toList()),
-            ),
-          ]),
-        ));
-  }
-
-  Widget _buildPointCard(Point point) => PointCard(
-        title: point.title,
-        content: point.content,
-        imageUrl: point.imageUrl,
-      );
+  Widget build(BuildContext context) => 
+    Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: GridView.builder(
+        
+        itemCount: points.length,
+        itemBuilder: (context, index) => 
+          PointCard(
+            title: points[index].title,
+            content: points[index].content,
+            imageUrl: points[index].imageUrl,
+          ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: MediaQuery.of(context).size.width / (MediaQuery.of(context).size.width / 0.5)
+        ),
+      )
+    );
 }
-
-// points.map((point) =>
-//   PointCard(
-//     content: point.content,
-//     title: point.title
-//   )
-// ).toList()
