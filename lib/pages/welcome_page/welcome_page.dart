2d297ba45ded1de8e3eab20971240ba2c3dae804
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-07-11 03:05:35 -0700

    feat: Created welcome_page.

---
+++ lib/pages/welcome_page/welcome_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  PageController _controller;
+  int _currentPage = 1;
+  final pages = <Widget>[
+    Container(width: double.infinity, color: Colors.green),
+    Container(width: double.infinity, color: Colors.red),
+  ];
+
+  void _onPageChanged(int page) =>
+    setState(() => _currentPage = page);
+
+  Widget _buildPage(BuildContext context, int index) => pages[index];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(
+      initialPage: _currentPage,
+      keepPage: false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: _controller,  
+      onPageChanged: _onPageChanged,
+      itemBuilder: _buildPage,
+    );
+  }
+}
