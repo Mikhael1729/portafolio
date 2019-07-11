de3ba9888ae82aae1c73a788960f7da34315f36d
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-07-10 22:05:10 -0700

    feat: Added "clase 2"

--- lib/models/topics.dart
+++ lib/models/topics.dart
@@ -3,6 +3,8 @@ import 'package:portafolio/models/resource.dart';
 import 'package:portafolio/models/resource_type.dart';
 import 'package:portafolio/models/url_resource.dart';
 
+import 'concept.dart';
+
 final classTopics = <ClassTopic>[
   // Clase 1
   ClassTopic(
@@ -21,4 +23,25 @@ final classTopics = <ClassTopic>[
   ),
 
   // Clase 2
+  ClassTopic(
+    id: 2,
+    title: "Álgebra booleana y circuitos lógicos",
+    content: "El álgebra booleana es una estructura algebraica que esquematiza las operaciones lógicas. En clase estuvimos viendo también lo que son puertas lógicas, que no son más que las aplicaciones básicas para con ellas formar circuitos lógicos. Existen varios métodos para para calcular el resultado de un conjunto de compuertas lógicas: Reducción de funciones y mapas de karnot",
+    coverImageUrl: "https://thumbs.subefotos.com/9997eee327fd1bc025f17970ec70eedco.jpg",
+    attachments: <Resource>[
+      Resource(
+        id: 2,
+        name: "Tarea 1",
+        description: "Convertir de decimal a binario",
+        type: ResourceType.externalImage,
+        url: "https://thumbs.subefotos.com/8ac1f349de233799c396c5128add7d58o.jpg"
+      )
+    ],
+    concepts: <Concept>[
+      Concept(
+        name: "Mapa de Karnaugh",
+        definition: "Es un diagrama utilizado para la simplificación de funciones algebraicas Booleanas. Fue creado por el físico-matemático Maurice Karnaugh"
+      )
+    ],
+  )
 ];
