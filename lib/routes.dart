65daa3c6c468d57dbaad9894fb6bf8b52de74080
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-07-12 19:22:06 -0700

    refacotor: Move pagesto main_layout.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
 import 'package:portafolio/pages/home_page/class_topic_page/class_topic_page.dart';
-import 'package:portafolio/pages/home_page/pages.dart';
 import 'package:portafolio/pages/welcome_page/welcome_page.dart';
 import 'layouts/main_layout/main_layout.dart';
+import 'layouts/main_layout/pages.dart';
 
 final routes = {
   '/': (context) => WelcomePage(),
